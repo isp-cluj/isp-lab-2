270628352f3068c204ae719a53a1bc32e43c5079
Marius <pascalaumarius3@gmail.com>
2020-02-17 18:48:01 +0200

    add Exercise 4

--- src/main/java/isp/lab2/Exercise4.java
+++ src/main/java/isp/lab2/Exercise4.java
@@ -1,4 +1,27 @@
 package isp.lab2;
 
 public class Exercise4 {
+
+    public static int firstOdd(int[] someNumbers) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static int firstEven(int[] someNumbers) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static int firstPrime(int[] someNumbers) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Don't forget to also display the position of ech number
+     */
+    public static void main(String[] args) {
+        int[] someNumbers = new int[]{15, 18, 13, 22, 21, 11, 57, 141, 563, 16};
+
+        System.out.println("First odd number: " + firstOdd(someNumbers));
+        System.out.println("First even number: " + firstEven(someNumbers));
+        System.out.println("First prime number: " + firstPrime(someNumbers));
+    }
 }
---
+++ src/test/java/isp/lab2/Exercise4Test.java
@@ -0,0 +1,25 @@
+package isp.lab2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Exercise4Test {
+
+    int[] someNumbers = new int[]{15, 18, 13, 22, 21, 11, 57, 141, 563, 16};
+
+    @Test
+    public void testFirstOdd(){
+        assertEquals(15,Exercise4.firstOdd(someNumbers));
+    }
+
+    @Test
+    public void testFirstEven(){
+        assertEquals(18,Exercise4.firstOdd(someNumbers));
+    }
+
+    @Test
+    public void testFirstPrime(){
+        assertEquals(13,Exercise4.firstOdd(someNumbers));
+    }
+}
