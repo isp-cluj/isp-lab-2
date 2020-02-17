bd0c66d860c57d923a76c71e9894bd998adb57cc
Marius <pascalaumarius3@gmail.com>
2020-02-17 19:02:40 +0200

    add Exercise 5

--- src/main/java/isp/lab2/Exercise5.java
+++ src/main/java/isp/lab2/Exercise5.java
@@ -1,4 +1,19 @@
 package isp.lab2;
 
 public class Exercise5 {
+
+    public static int[] generateRandomNumbers() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static int[] getSortedNumbers(int[] randomNumbers) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void main(String[] args) {
+        int[] sortedNumbers = getSortedNumbers(generateRandomNumbers());
+        for (int i = 0; i < sortedNumbers.length; i++) {
+            System.out.print(sortedNumbers[i] + " ");
+        }
+    }
 }
